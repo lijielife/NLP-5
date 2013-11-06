2323eb1f3e7e41e6c299bcae9a151b2834429bd2
Sahil Bajaj <sahil29@gmail.com>
2013-11-06 22:05:15 +0800

    Range stuff added in (put files in a separate training directory)

--- parsing/syntactic_parser.sh
+++ parsing/syntactic_parser.sh
@@ -4,8 +4,12 @@ java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentence
 
 # Command 2
 
-java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -PCFG -headFinder edu.stanford.nlp.trees.LeftHeadFinder -sentences newline -train "train_1" -saveToSerializedFile "SerializedModel1" -writeOutputFiles "AllPlainTextTweets"
+java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -PCFG -headFinder edu.stanford.nlp.trees.LeftHeadFinder -sentences newline -train "path/to/training/data" 1 -saveToSerializedFile "SerializedModel1" -writeOutputFiles "PlainTextTweets_subset1"
 
 # Command 3
 
 java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline -tokenized -tagSeparator _ -tokenizerFactory edu.stanford.nlp.process.WhitespaceTokenizer -tokenizerMethod newCoreLabelTokenizerFactory -loadFromSerializedFile "SerializedModel1" "set2_corrected" > "output_2_pcfg"
+
+# Command 4 (same as command 2, with the range modified - change the range as you move on)
+
+java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -PCFG -headFinder edu.stanford.nlp.trees.LeftHeadFinder -sentences newline -train "path/to/training/data" 1-2 -saveToSerializedFile "SerializedModel1" -writeOutputFiles "PlainTextTweets"
