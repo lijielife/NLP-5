f1f2b1a81cecb120ca571006c13956dbbfca62d5
Sahil Bajaj <sahil29@gmail.com>
2013-11-06 20:16:03 +0800

    Added commands for syntactic parser

---
+++ parsing/syntactic_parser.sh
@@ -0,0 +1,11 @@
+# Command 1
+
+java -cp "*" -mx2g edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline -tokenized -tagSeparator _ -tokenizerFactory edu.stanford.nlp.process.WhitespaceTokenizer -tokenizerMethod newCoreLabelTokenizerFactory edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz "set1_corrected" > "output_1_pcfg"
+
+# Command 2
+
+java -cp "*" -mx2g edu.stanford.nlp.parser.lexparser.LexicalizedParser -PCFG -headFinder edu.stanford.nlp.trees.LeftHeadFinder -sentences newline "train_1" -writeOutPutFiles "AllPlainTextTweets" -saveToSerializedFile "SerializedModel1"
+
+# Command 3
+
+java -cp "*" -mx2g edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline -tokenized -tagSeparator _ -tokenizerFactory edu.stanford.nlp.process.WhitespaceTokenizer -tokenizerMethod newCoreLabelTokenizerFactory -loadFromSerializedFile "SerializedModel1" "set2_corrected" > "output_2_pcfg"
