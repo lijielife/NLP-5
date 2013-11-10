80eeaade6c4d80ef3f9d3feffbfa3f0242724951
Sahil Bajaj <sahil29@gmail.com>
2013-11-11 00:09:03 +0800

    Add more rules to normaliser for treating newlines, unicode and smilies with noses

--- normalise.rb
+++ normalise.rb
@@ -7,24 +7,30 @@ write_file = File.new(filename_write, "w")
 
 count = 0
 while (line = read_file.gets)
-	# 1. Replace escaped quotes with just quotes
+	# Replace newlines with periods
+	line.gsub!(/\\n/, '.')
+
+	# Remove unicode entities
+	line.gsub!(/\\u.{4}/, '')
+
+	# Replace escaped quotes with just quotes
 	line.gsub!(/\\"/, '"')
 
-	# 2. Replace a series of more than one periods, exclamation makrs, or
+	# Replace a series of more than one periods, exclamation makrs, or
 	# questions marks with a period and a space
 	line.gsub!(/([.?!]){2,}/, '\1 ')
 
-	# 3. Remove 'RT's and 'Retweet's
+	# Remove 'RT's and 'Retweet's
 	line.gsub!(/\bRT\b/i, '')
 	line.gsub!(/retweet/i, '')
 
-	# 4. Remove @mentions (@replies) occurring at the start of the tweet
+	# Remove @mentions (@replies) occurring at the start of the tweet
 	line.gsub!(/^@.*? /, '')
 
-	# 5. Remove smilies and sadlies
-	line.gsub!(/:[()]/, '')
+	# Remove smilies and sadlies
+	line.gsub!(/:-?[()]/, '')
 
-	# 6. Put sentences on new lines
+	# Put sentences on new lines
 	line.gsub!(/([.?!] )/, "\\1\n")
 
 	print line
