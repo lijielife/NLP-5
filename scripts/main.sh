8ab0e06af7ab7695997de4a6e76d1b110f69a3e9
Sahil Bajaj <sahil29@gmail.com>
2013-11-15 07:21:55 +0800

    Add commands for POS tagging and parsing

--- scripts/main.sh
+++ scripts/main.sh
@@ -13,7 +13,11 @@ echo -n "Normalising data... "
 echo "Done."
 
 echo -n "Adding part-of-speech tags... "
+cd twitie-tagger/
+java -jar twitie_tag.jar ../Training\ Data/POS\ Tagger/Training\ POS\ Models/finalpos.tagger ../tweets.plaintext.normal > ../tweets.plaintext.normal.pos
+cd ..
 echo "Done."
 
 echo -n "Parsing POS-tagged data... "
+java -cp "*" -mx1g edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences newline -tokenized -tagSeparator _ -tokenizerFactory edu.stanford.nlp.process.WhitespaceTokenizer -tokenizerMethod newCoreLabelTokenizerFactory -loadFromSerializedFile Training\ Data/Parser/serialized_models/SerializedModel10 tweets.plaintext.normal.pos > tweets.plaintext.normal.pos.parsed
 echo "Done."
